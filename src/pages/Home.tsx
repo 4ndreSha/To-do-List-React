77e2454616cf101cd5f63eac0c414989657c93ba
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-08-21 23:57:19 +0300

    ref: project structure and dependencies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TodoWrapper } from '../components/TodoWrapper';
 
 function Home() {
   return (
     <>
-      <div class="task-list">
-        <Link class='link' to='/task1'>Task 1</Link>
-        <Link class='link' to='/task2'>Task 2</Link>
-        <Link class='link' to='/task3'>Task 3</Link>
-      </div>
+      <TodoWrapper/>
     </>
   );
 }
