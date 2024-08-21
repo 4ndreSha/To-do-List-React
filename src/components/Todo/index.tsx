ff3b48028c4af74179056ce54ab4718393f99afb
4ndreSha <83921009+4ndreSha@users.noreply.github.com>
2024-08-22 00:19:22 +0300

    feat: creating & visualizing todos logic

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
-export const Todo = () => {
+
+export const Todo = ({ task }) => {
     return (
-        
+        <div className='Todo'>
+            <p>{task.text}</p>
+            <div>
+                <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+                <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+            </div>
+        </div>
     )
 }
--- src/components/TodoWrapper/index.tsx
+++ src/components/TodoWrapper/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import { useState } from 'react';
+import { TodoForm } from '../TodoFrom';
+import { Todo } from '../Todo';
+import { v4 as uuidv4 } from 'uuid'; 
+
+uuidv4();
 
 export const TodoWrapper = () => {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+        setTodos([...todos, {
+            id: uuidv4(),
+            text: todo,
+            completed: false,
+            isEditing: false
+        }]);
+    }
+
     return (
-        
+        <div className='TodoWrapper'>
+            <TodoForm addTodo={addTodo}/>
+            {todos.map((item) => (
+                <Todo task={item}/>
+            ))}
+        </div>
     )
 }
